ca61bc895a2d1b93a4742d64b23e31f3f7f368a5
dinyt <angel_of_life@mail.ru>
2021-05-18 14:17:50 +0300

    Задание 26. Исправления

--- fixture/soap.py
+++ fixture/soap.py
@@ -8,7 +8,7 @@ class SoapHelper:
         self.app = app
 
     def can_login(self, username, password):
-        client = Client("http://localhost/mantisbt-2.25.0/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.config["web"]["baseUrl"] + "/api/soap/mantisconnect.php?wsdl")
         try:
             client.service.mc_login(username, password)
             return True
@@ -16,7 +16,7 @@ class SoapHelper:
             return False
 
     def get_all_projects(self, username, password):
-        client = Client("http://localhost/mantisbt-2.25.0/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.config["web"]["baseUrl"] + "/api/soap/mantisconnect.php?wsdl")
         list = []
         try:
             res = client.service.mc_projects_get_user_accessible(username, password)
